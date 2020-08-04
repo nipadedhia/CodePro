35b7a508ccc296bbc8e8d1854d083501a4b5ce28
Nipa Dedhia <nipa.dedhia@gmail.com>
2020-08-04 13:52:34 -0500

    initial server.js file

---
+++ server.js
@@ -0,0 +1,42 @@
+// Dependencies and modules
+const express = require("express");
+const path = require("path");
+// Tells node that we are creating an express server
+const app = express();
+// Calls mongoose an odm for MongoDB
+const mongoose = require("mongoose");
+// Sets an initial PORT that will listen for requests from the client side
+const PORT = process.env.PORT || 3001;
+const routes = require("./routes");
+
+// Middleware = function that will be executed in between request and responses
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static(path.join(__dirname, "public")));
+
+// Api routes will go here
+app.use(routes);
+mongoose.connect(
+  process.env.MONGODB_URI || "mongodb://localhost/coursedb",
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+  },
+  () => console.log("Connected to the the DB!"),
+);
+
+// Send every other request to the React app
+// app.get("*", (req, res) => {
+//   res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+// });
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client", "index.html"));
+});
+
+// Starts the server and syncing models
+
+app.listen(PORT, () => {
+  console.log(`Server listening on port http://localhost:${PORT}`);
+});
