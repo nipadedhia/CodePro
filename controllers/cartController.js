b9ed1d56e7580d1b7b2425791d5512ee888e9b8f
Nipa Dedhia <nipa.dedhia@gmail.com>
2020-08-04 21:00:14 -0500

    defining method for cart controller

--- controllers/cartController.js
+++ controllers/cartController.js
@@ -1,6 +1,6 @@
 const db = require("../models");
 
-// Defining methods for the products controller
+// Defining methods for the Cart controller
 module.exports = {
   findAll: function (req, res) {
     db.Cart.find(req.query)
